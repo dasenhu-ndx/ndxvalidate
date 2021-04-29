ecefedfe7836fa0359529d82cd64d9a07a7fc471
molowuadmin <olowu.marydan@gmail.com>
2021-04-29 16:35:03 -0500

    dded prop

--- src/main/java/com/ndx/ndxvalidate/data/entity/TestEntity.java
+++ src/main/java/com/ndx/ndxvalidate/data/entity/TestEntity.java
@@ -1,4 +1,33 @@
 package com.ndx.ndxvalidate.data.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "NDX_Validate_TestTable")
 public class TestEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "TestName")
+    private String testName;
+
+    @Column(name = "LabTestNote")
+    private String labTestNote;
+
+    @Column(name = "Doctor")
+    private String doctor;
+
+    @Column(name = "Customer")
+    private String customer;
+
+    @Column(name = "Mode")
+    private int mode;
+
+
+
+
+
 }

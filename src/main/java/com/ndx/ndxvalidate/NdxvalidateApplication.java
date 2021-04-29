c03ac266d3b375812682b898dba34077841c36a1
Mary Olowu <olowu.marydan@gmail.com>
2021-04-29 12:24:20 -0500

    This is the first commit for the Ndx Validate project

---
+++ src/main/java/com/ndx/ndxvalidate/NdxvalidateApplication.java
@@ -0,0 +1,13 @@
+package com.ndx.ndxvalidate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdxvalidateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NdxvalidateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndx/ndxvalidate/NdxvalidateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndx.ndxvalidate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdxvalidateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

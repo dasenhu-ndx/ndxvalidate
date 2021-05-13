674092b0b113d6617339375e9e850fc19f95efde
Mary Olowu <olowu.marydan@gmail.com>
2021-05-13 11:03:28 -0500

    Test LMS Dashboard

---
+++ src/main/java/com/ndx/ndxvalidate/business/service/AdminService.java
@@ -0,0 +1,26 @@
+package com.ndx.ndxvalidate.business.service;
+
+
+import com.ndx.ndxvalidate.data.entity.Admin;
+import com.ndx.ndxvalidate.data.repository.AdminRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+
+    private final AdminRepo adminRepo;
+
+
+    public AdminService(AdminRepo adminRepo) {
+        this.adminRepo = adminRepo;
+    }
+
+    public Boolean checkAdminPrivilege(String mtUserName){
+
+        Admin admin = adminRepo.findAdminByMtUserName(mtUserName);
+
+        if (admin == null){
+            return false;
+        } else return true;
+    }
+}
--- src/main/java/com/ndx/ndxvalidate/controller/AppController.java
+++ src/main/java/com/ndx/ndxvalidate/controller/AppController.java
@@ -1,19 +1,52 @@
 package com.ndx.ndxvalidate.controller;
 
+import com.ndx.ndxvalidate.business.service.AdminService;
+import com.ndx.ndxvalidate.business.service.MTUserService;
 import com.ndx.ndxvalidate.data.entity.AccountRequest;
 import com.ndx.ndxvalidate.data.entity.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class AppController {
 
+    private  final MTUserService mtUserService;
+    private final AdminService adminService;
+
+    @Autowired
+    public AppController(MTUserService mtUserService, AdminService adminService) {
+        this.mtUserService = mtUserService;
+        this.adminService = adminService;
+    }
+
+
     @GetMapping("/home")
-    public String getHomePage(){
+    public  String getIndex(Model model){
+        String mtUserName = mtUserService.currentUserName();
+        Boolean userStatus = adminService.checkAdminPrivilege(mtUserName);
+
+        model.addAttribute("userPermission", userStatus);
 
         return "index";
     }
+
+    @GetMapping("/lms")
+    public String getLmsDashboard(){
+
+        String mtUserName = mtUserService.currentUserName();
+        Boolean userStatus = adminService.checkAdminPrivilege(mtUserName);
+        System.out.println(userStatus);
+        if(userStatus == true) {
+            return "lms_portal";
+        }else{
+            return "no_access";
+        }
+    }
+
+
 
     @GetMapping("/test")
     public String getTestPage(){
